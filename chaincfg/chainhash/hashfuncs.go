2fb07940ea4a6178e0f53569cd7d944147e65d6e
Samuel Robinson <samuel.robinson@tangazzoo.com>
2018-04-02 00:17:05 +0100

    keccak implementation

--- chaincfg/chainhash/hashfuncs.go
+++ chaincfg/chainhash/hashfuncs.go
@@ -5,29 +5,52 @@
 
 package chainhash
 
-import "crypto/sha256"
+
+import (
+	"github.com/ProjectVisor/xvrd/keccak"
+)
 
 // HashB calculates hash(b) and returns the resulting bytes.
 func HashB(b []byte) []byte {
-	hash := sha256.Sum256(b)
+
+
+
+
+	h := keccak.New256()
+	h.Write(b)
+	hash := h.Sum(nil)
+
 	return hash[:]
 }
 
 // HashH calculates hash(b) and returns the resulting bytes as a Hash.
 func HashH(b []byte) Hash {
-	return Hash(sha256.Sum256(b))
+	h := keccak.New256()
+	h.Write(b)
+	return Hash(h.Sum(nil))
 }
 
 // DoubleHashB calculates hash(hash(b)) and returns the resulting bytes.
 func DoubleHashB(b []byte) []byte {
-	first := sha256.Sum256(b)
-	second := sha256.Sum256(first[:])
+
+	h := keccak.New256()
+	h.Write(b)
+	first := h.Sum(nil)
+
+	h := keccak.New256()
+	h.Write(first[:])
+	second := h.Sum(nil)
 	return second[:]
 }
 
 // DoubleHashH calculates hash(hash(b)) and returns the resulting bytes as a
 // Hash.
 func DoubleHashH(b []byte) Hash {
-	first := sha256.Sum256(b)
-	return Hash(sha256.Sum256(first[:]))
+
+	h := keccak.New256()
+	h.Write(b)
+	first := h.Sum(nil)
+	h := keccak.New256()
+	h.Write(first[:])
+	return Hash(h.Sum(nil))
 }
